f1de6c459015084c6843bb58de45ac284896e430
shuichi <shu.heihei1209@gmail.com>
2020-04-20 20:16:21 +0900

    finish3-8

--- vagrant/sample_app/config/initializers/application_controller_renderer.rb
+++ vagrant/sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '884698d36e40aa4ddc9c68c4cfdbd68b8bfb546d49fda923e8ff875948b783a92cb6f0332f2ea84311d5119508c4ba1a350966773b7dcf9190c506fbb74bcb65'
