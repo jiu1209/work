734fbef9781e450259e31c8a15745fd6bef66033
shuichi <shu.heihei1209@gmail.com>
2020-04-20 18:09:40 +0900

    finish3-6

--- vagrant/sample_app/app/controllers/todolists_controller.rb
+++ vagrant/sample_app/app/controllers/todolists_controller.rb
@@ -2,17 +2,23 @@ class TodolistsController < ApplicationController
   def new
   	@list = List.new
   end
-
   def create
-  	list = List.new(list_params)
+        # ストロングパラメーターを使用
+         list = List.new(list_params)
+        # DBへ保存する
+         list.save
+         redirect_to todolist_path(list.id)
+    end
 
-  	list.save
+    def index 
+         @lists = List.all
+    end
 
-   	redirect_to '/top'
-   end
-   private
-
-   def list_params
-   	   params.require(:list).permit(:title, :body)
-   end
+    def show
+        @list = List.find(params[:id]) 
+    end
+    private
+    def list_params
+        params.require(:list).permit(:title, :body)
+    end
 end
