dc6bc2467781f955f47d681c44fa03e505d6065b
shuichi <shu.heihei1209@gmail.com>
2020-04-22 12:41:05 +0900

    finish4-4

--- vagrant/CARAVAN/app/controllers/blogs_controller.rb
+++ vagrant/CARAVAN/app/controllers/blogs_controller.rb
@@ -1,24 +1,28 @@
 class BlogsController < ApplicationController
-  def index
-  end
-
+ 
   def show
+     @blog = Blog.find(params[:id])
   end
-
+ 
+  def index
+  	 @blogs = Blog.all
+  end
+ 
   def new
   	  @blog = Blog.new
   end
-  def edit
+ 
+  def create
+      blog = Blog.new(blog_params)
+      blog.save
+      redirect_to blog_path(blog.id)
   end
 
-  def create
-  	  blog = Blog.new(blog_params)
-  	         blog.save
-  	         redirect_to blogs_pach
+  def edit
   end
 
   private
   def blog_params
-  	params.requir(:blog).permit(:title, :category, :body)
+      params.require(:blog).permit(:title, :category, :body)
   end
- end
+end
